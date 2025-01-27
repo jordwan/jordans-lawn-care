3719db231311fb347046b68dc8fb533e60756e04
jordwan <94031022+jordwan@users.noreply.github.com>
2025-01-26 18:26:24 -0800

    delete '

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Jordan's Lawn Care
+        Jordans Lawn Care
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         Call Jordan @ 250-555-6969 today for all of your lawn care needs
