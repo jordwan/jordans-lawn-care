a55f3dbcdedeca4dd3789cf1cfe49cd4141e9daf
jordwan <94031022+jordwan@users.noreply.github.com>
2025-02-02 13:30:18 -0800

    changing structure and adding home components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
+import { Rethink_Sans } from "next/font/google";
 import "./globals.css";
+
+const font = Rethink_Sans({
+  weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Jordan's Lawn Care",
@@ -14,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${font.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-export default function Home() {
+import Home from "@/components/Home/Home";
+import React from "react";
+
+const HomePage = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Jordans Lawn Care
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        Call Jordan @ 250-555-6969 today for all of your lawn care needs
-      </footer>
+    <div>
+      <Home />
     </div>
   );
-}
+};
+
+export default HomePage;
+
+// Localhost:3000
+// Localhost:3000/services
+// Localhost:3000/contact
